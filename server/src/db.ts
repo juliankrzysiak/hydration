1cba0a63080904461a785cce16f4fd9499df590a
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-05-29 15:22:44 -0700

    Add first test

--- server/src/db.ts
+++ server/src/db.ts
@@ -2,7 +2,7 @@ import config from './utils/config.js';
 import postgres from 'postgres';
 
 const sql = postgres(
-	`postgres://${config.DB_USER}:${config.DB_PASSWORD}.db.elephantsql.com/${config.DB_USER}`
+	`postgres://${config.DB_USER}:${config.DB_PASSWORD}@mahmud.db.elephantsql.com/${config.DB_USER}`
 );
 
 export default sql;
--- server/src/tests/plants.test.ts
+++ server/src/tests/plants.test.ts
@@ -1,2 +1,9 @@
 import supertest from 'supertest';
 import app from '../app';
+import { it } from 'vitest';
+
+const api = supertest(app);
+
+it('returns purple sage', async () => {
+	await api.get('/api/plants').expect(200);
+});
