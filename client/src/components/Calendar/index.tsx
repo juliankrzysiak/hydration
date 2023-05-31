e35b208123a808f8996571c9c75e840fba62ec0e
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-05-30 21:26:19 -0700

    Add calendar badges based on data

--- client/src/components/Calendar/index.tsx
+++ client/src/components/Calendar/index.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { Plant } from "../../types";
 import { default as DateCalendar } from "react-calendar";
 import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
+dayjs.extend(customParseFormat);
 
 type ValuePiece = Date | null;
 
@@ -15,25 +17,20 @@ interface Args {
 export default function Calendar({ plants }: { plants: Plant[] }) {
   const [value, onChange] = useState<Value>(new Date());
 
-  console.log(plants);
-
   function tileContent({ date, view }: Args) {
     if (view === "month") {
-      plants.forEach((plant) => {
-        if (
-          plant.watered.find((dDate: Date) => dayjs(dDate).isSame(dayjs(date)))
-        ) {
-          return (
-            <svg
-              viewBox="0 0 100 100"
-              xmlns="http://www.w3.org/2000/svg"
-              className="absolute right-0 top-0 m-1  w-2 fill-gray-700"
-            >
-              <circle cx="50" cy="50" r="50" />
-            </svg>
-          );
-        }
-      });
+      const allNextDate = plants.map((plant) => plant.next_water);
+      if (allNextDate.find((dDate) => dayjs(dDate).isSame(date, "day"))) {
+        return (
+          <svg
+            viewBox="0 0 100 100"
+            xmlns="http://www.w3.org/2000/svg"
+            className="absolute right-0 top-0 m-1  w-2 fill-gray-700"
+          >
+            <circle cx="50" cy="50" r="50" />
+          </svg>
+        );
+      }
     }
   }
 
