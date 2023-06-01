fe3c9c25617b248d909476f3d950d9d244068381
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-01 14:04:48 -0700

    Add react router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,14 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 
 const queryClient = new QueryClient();
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} />
     </QueryClientProvider>
   </React.StrictMode>
 );
