e2b062dccec728c5b216068f7e58d86c4d2d5629
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-06 19:54:55 -0700

    Style buttons to be centered

--- client/src/components/Menu/MenuButton.tsx
+++ client/src/components/Menu/MenuButton.tsx
@@ -1,19 +1,29 @@
+import { motion } from "framer-motion";
+
 interface Props {
   ADD?: boolean;
+  DELETE?: boolean;
   direction: string;
   handleClick: () => void;
 }
 
-export const MenuButton = ({ ADD, handleClick, direction }: Props) => {
+export const MenuButton = ({ ADD, DELETE, handleClick, direction }: Props) => {
   let type;
   if (ADD) type = "plus";
+  if (DELETE) type = "cancel";
 
   return (
-    <button
-      onClick={handleClick}
-      className={`aspect-square w-12 rounded-xl border-2 border-gray-900 bg-gray-500/10 p-1 transition-all duration-200 ${direction}`}
+    <div
+      className={`absolute grid aspect-square w-16 place-content-center transition-all duration-200 ${direction}`}
     >
-      <img src={`/${type}.svg`} alt={type} />
-    </button>
+      <motion.button
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        className="aspect-square w-12 rounded-xl border-2 border-gray-900 bg-gray-500/10 p-1"
+        onClick={handleClick}
+      >
+        <img src={`/${type}.svg`} alt={type} />
+      </motion.button>
+    </div>
   );
 };
