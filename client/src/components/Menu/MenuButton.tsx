419a54a9aaabf3cfee23944776dbffd0273cfb50
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-06 18:44:02 -0700

    Refactor menu buttons for legibility

--- client/src/components/Menu/MenuButton.tsx
+++ client/src/components/Menu/MenuButton.tsx
@@ -1,22 +1,19 @@
 interface Props {
-  pressed: boolean;
+  ADD?: boolean;
   direction: string;
-  handleClick: (value: boolean) => void;
+  handleClick: () => void;
 }
 
-export const MenuButton = ({ pressed, direction, handleClick }: Props) => {
+export const MenuButton = ({ ADD, handleClick, direction }: Props) => {
+  let type;
+  if (ADD) type = "plus";
+
   return (
     <button
-      onClick={() => handleClick(true)}
-      className={`absolute -z-10  aspect-square w-12 rounded-full border-2 border-gray-900 bg-gray-500/10 p-1 transition-all duration-200 
-        ${
-          pressed
-            ? `${direction} let-1/2  visible opacity-100`
-            : "invisible opacity-0"
-        }
-        `}
+      onClick={handleClick}
+      className={`aspect-square w-12 rounded-xl border-2 border-gray-900 bg-gray-500/10 p-1 transition-all duration-200 ${direction}`}
     >
-      <img src="/plus.svg" alt="Plus" />
+      <img src={`/${type}.svg`} alt={type} />
     </button>
   );
 };
--- client/src/components/Menu/index.tsx
+++ client/src/components/Menu/index.tsx
@@ -1,26 +1,42 @@
 import { useState } from "react";
 import { MenuButton } from "./MenuButton";
-import { CreateButton } from "./CreateButton";
+import { motion } from "framer-motion";
+import { useShowStore } from "../../store";
+
+const translate = {
+  N: "-translate-y-16",
+  NW: "-translate-y-16 -translate-x-16",
+  W: "-translate-x-16",
+};
 
 export const Menu = () => {
   const [pressed, setPressed] = useState(false);
-  const translate = {
-    N: "-translate-y-16",
-    NW: "-translate-y-16 -translate-x-16",
-    W: "-translate-x-16",
+
+  const addPlant = () => {
+    useShowStore.setState({ createForm: true });
+    setPressed(false);
   };
 
   return (
     <nav className="fixed bottom-6 right-6 grid place-content-center">
       <button
-        className="aspect-square w-14 rounded-full border-2 border-gray-900 bg-gray-300 p-1"
+        className="aspect-square w-16 rounded-2xl border-2 border-gray-900 bg-gray-300/20 p-1"
         aria-pressed={pressed}
         onClick={() => setPressed(!pressed)}
       >
         <img src={`/${pressed ? "cancel" : "menu"}.svg`} alt="Hamburger menu" />
       </button>
-      <CreateButton pressed={pressed} direction={translate.N} setPressed={setPressed}/>
-      {/* <MenuButton pressed={pressed} direction={translate.NW} /> */}
+      {pressed && (
+        <motion.div
+          className="absolute left-1/2 top-1/2 -z-10 -translate-x-1/2 -translate-y-1/2"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+        >
+          <MenuButton ADD handleClick={addPlant} direction={translate.N} />
+        </motion.div>
+      )}
+
+      {/* <MenuButton pressed={pressed} direction={translate.W} /> */}
       {/* <MenuButton pressed={pressed} direction={translate.W} /> */}
     </nav>
   );
