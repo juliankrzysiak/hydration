4a4d8f0a8ce8ea82e7096dd8aa7e8f8417abf00f
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-10 13:48:06 -0700

    Add redirect to login if not logged in

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,18 +3,29 @@ import ReactDOM from "react-dom/client";
 import { Home } from "@/features/calendar/routes/Home";
 import { ErrorPage } from "@/routes/ErrorPage";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  redirect,
+  RouterProvider,
+} from "react-router-dom";
 import "./index.css";
 import { Login } from "@/features/auth/routes/Login";
 import { LoginForm } from "@/features/auth/components/Forms/LoginForm";
 import { RegisterForm } from "@/features/auth/components/Forms/RegisterForm";
+import { supabase } from "@/features/auth/lib/auth";
 
 const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/home",
     element: <Home />,
+    loader: async () => {
+      const session = await supabase.auth.getSession();
+      console.log(session);
+      if (!session) return redirect("account/login");
+      return null;
+    },
     errorElement: <ErrorPage />,
   },
   {
