71ea9699d00b4f2e72a2b8be527b91c367b06b65
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-10 00:01:22 -0700

    Update notification with another variant

--- client/src/components/Notification/index.tsx
+++ client/src/components/Notification/index.tsx
@@ -4,17 +4,18 @@ import { motion, AnimatePresence } from "framer-motion";
 import cancel from "@/assets/cancel.svg";
 import alert from "@/assets/alert.svg";
 import info from "@/assets/info.svg";
+import checkmarkCircle from "@/assets/checkmark-circle-outline.svg";
 
 export const Notification = () => {
   const message = useNotificationStore((state) => state.message);
   const type = useNotificationStore((state) => state.type);
 
-  // useEffect(() => {
-  //   const timeout = setTimeout(() => {
-  //     useNotificationStore.setState({ message: "" });
-  //   }, 5000);
-  //   return () => clearTimeout(timeout);
-  // }, [message]);
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      useNotificationStore.setState({ message: "" });
+    }, 5000);
+    return () => clearTimeout(timeout);
+  }, [message]);
 
   return (
     <AnimatePresence>
@@ -23,13 +24,17 @@ export const Notification = () => {
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className={` fixed bottom-7 left-1/2 z-30 flex  max-w-xs -translate-x-1/2 rounded-xl border border-gray-500 py-2 pl-4 pr-2 ${
-            type === "info" ? "bg-green-200" : " bg-red-200"
-          }`}
+          className={` fixed bottom-7 left-1/2 z-30 flex  max-w-xs -translate-x-1/2 rounded-xl border border-gray-500 py-2 pl-4 pr-2
+           ${type === "info" && "bg-neutral-200"}
+           ${type === "error" && "bg-red-200"}
+           ${type === "action" && "bg-green-200"}`}
         >
-          {type === "error" && <img className="w-8" src={alert} alt="Alert!" />}
           {type === "info" && (
             <img className="w-8" src={info} alt="Information" />
+          )}
+          {type === "error" && <img className="w-8" src={alert} alt="Alert!" />}
+          {type === "action" && (
+            <img className="w-8" src={checkmarkCircle} alt="Checkmark" />
           )}
 
           <p className="ml-3 w-52 font-['Inter']">{message}</p>
--- client/src/stores/notificationStore.ts
+++ client/src/stores/notificationStore.ts
@@ -2,10 +2,10 @@ import { create } from "zustand";
 
 interface State {
   message: string;
-  type: "info" | "error";
+  type: "info" | "error" | "action";
 }
 
 export const useNotificationStore = create<State>(() => ({
   message: "Password should be at least 6 characters long",
-  type: "error",
+  type: "info",
 }));
