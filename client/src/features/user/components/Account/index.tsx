90e949b87178f29f527a8e928a5830cd85ac0e09
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-12 17:41:19 -0700

    Add basic structure for account settings

--- client/src/features/user/components/Account/index.tsx
+++ client/src/features/user/components/Account/index.tsx
@@ -1,9 +1,18 @@
 export const Account = () => {
   return (
-    <form>
-      <div>
-        <label htmlFor=""></label>
-      </div>
+    <form className="flex flex-col gap-4">
+      <p>Make changes to your account here</p>
+      <fieldset className=" flex flex-col">
+        <label htmlFor="name">First Name</label>
+        <input className="rounded-md" type="text" />
+      </fieldset>
+      <fieldset className="mb-2 flex flex-col">
+        <label htmlFor="email">Email</label>
+        <input type="email" />
+      </fieldset>
+      <button className="self-end rounded border-2 border-gray-800 px-2 py-2">
+        Save Changes
+      </button>
     </form>
   );
 };
--- client/src/features/user/components/Tabs/index.tsx
+++ client/src/features/user/components/Tabs/index.tsx
@@ -1,14 +1,20 @@
 import * as Tab from "@radix-ui/react-tabs";
+import { Account } from "../Account";
 
 export const Tabs = () => {
   return (
     <Tab.Root defaultValue="">
-      <Tab.List aria-label="Manage your account">
+      <Tab.List
+        aria-label="Manage your account"
+        className="flex justify-around"
+      >
         <Tab.Trigger value="tab1">Account</Tab.Trigger>
         <Tab.Trigger value="tab2">Password</Tab.Trigger>
         <Tab.Trigger value="tab3">Delete</Tab.Trigger>
       </Tab.List>
-      <Tab.Content value="tab1">stuff</Tab.Content>
+      <Tab.Content value="tab1" className="w-full rounded-xl bg-gray-300 p-4">
+        <Account />
+      </Tab.Content>
     </Tab.Root>
   );
 };
