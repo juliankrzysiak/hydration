ab186f0ecff8a45ea3fce749b2bad2e4646332b3
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-12 17:38:58 -0700

    Create hook to fetch user's name

--- client/src/features/user/routes/User.tsx
+++ client/src/features/user/routes/User.tsx
@@ -1,12 +1,14 @@
 import person from "@/assets/person.svg";
 import { Tabs } from "../components/Tabs";
+import { useName } from "../hooks/useName";
 
 export const User = () => {
+  const name = useName();
   return (
-    <main className="relative flex min-h-screen flex-col items-center justify-center bg-neutral-100">
-      <div className="flex flex-col">
+    <main className="relative flex min-h-screen flex-col items-center bg-neutral-100">
+      <div className="mb-4 flex flex-col items-center">
         <img className="w-12" src={person} alt="Person" />
-        <p>Hello, {"Julian"}</p>
+        <p>Hello, {name}</p>
       </div>
       <Tabs />
     </main>
