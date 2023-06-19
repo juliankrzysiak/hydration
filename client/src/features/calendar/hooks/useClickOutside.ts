d96306bb29bcb43420e0003c2a0f8d8d14347d97
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-18 19:19:24 -0700

    Add escape key to exit out of form

--- client/src/features/calendar/hooks/useClickOutside.ts
+++ client/src/features/calendar/hooks/useClickOutside.ts
@@ -1,19 +1,26 @@
 import { useEffect } from "react";
 
 export const useClickOutside = (
-  action: () => void,
+  remove: () => void,
   ref: React.RefObject<HTMLElement>
 ) => {
   useEffect(() => {
     const handleClickOutside = (event: MouseEvent) => {
       if (ref.current && event.target instanceof Element) {
-        if (!ref.current.contains(event.target)) action();
+        if (!ref.current.contains(event.target)) remove();
       }
     };
 
+    const handleEscapeOutside = (event: KeyboardEvent) => {
+      if (ref.current && event.key === "Escape") remove();
+    };
+
     document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("keydown", handleEscapeOutside);
+
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("keydown", handleEscapeOutside);
     };
-  }, [action, ref]);
+  }, [remove, ref]);
 };
