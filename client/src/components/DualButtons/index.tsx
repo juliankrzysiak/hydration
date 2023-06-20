a667f84362ecd3a8d8f99d276d087ee874c67b43
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-19 23:03:41 -0700

    Create general button component

---
+++ client/src/components/DualButtons/index.tsx
@@ -0,0 +1,28 @@
+import plusSVG from "@/assets/plus.svg";
+import cancelSVG from "@/assets/cancel.svg";
+
+interface Props {
+  confirm: () => void;
+  cancel: () => void;
+}
+
+export const DualButtons = ({ confirm, cancel }: Props) => {
+  return (
+    <div className="flex gap-4 self-center">
+      <button
+        className="rounded-md bg-gray-200/20 p-1"
+        aria-label="Show Add Date Form"
+        onClick={confirm}
+      >
+        <img className="w-5" src={plusSVG} alt="Plus" />
+      </button>
+      <button
+        className="rounded-md bg-gray-200/20 p-1"
+        aria-label="Show Delete Date Form"
+        onClick={cancel}
+      >
+        <img className="w-5" src={cancelSVG} alt="Cancel" />
+      </button>
+    </div>
+  );
+};
