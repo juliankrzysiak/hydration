1538f40bfb1807ee8e5ded2f17360636711d5899
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-26 21:19:09 -0700

    Add check for sessionStorage uid

--- client/src/features/calendar/utils/getUid.ts
+++ client/src/features/calendar/utils/getUid.ts
@@ -2,6 +2,10 @@ import { supabase } from "@/features/auth/lib/auth";
 
 export const getUid = async (): Promise<string> => {
   const { data } = await supabase.auth.getSession();
+
+  const anonUID = sessionStorage.getItem("uid");
+  if (anonUID) return anonUID;
+
   if (!data.session) throw new Error("Session does not exist, please log in.");
   return data.session.user.id;
 };
