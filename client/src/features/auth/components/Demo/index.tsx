b11f3df4b153cae257c3851bca2c6d1334fd19bf
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-26 20:18:18 -0700

    Associate anon account with random email
    Since supabse does not yet provide support for anonymous accounts, I
    will need to create a workaround, which will be associating the
    account with a fake email that is unknown to the user.

--- client/src/features/auth/components/Demo/index.tsx
+++ client/src/features/auth/components/Demo/index.tsx
@@ -1,6 +1,20 @@
+import { notify } from "@/utils/notify";
+import { supabase } from "../../lib/auth";
+
 export const Demo = () => {
+  const randomValues = crypto.getRandomValues(new Int8Array(8)).join("");
+  const createGuest = async () => {
+    const { data, error } = await supabase.auth.signUp({
+      email: `${randomValues}@invalid.com`,
+      password: "testing123",
+    });
+    if (error) notify("error", error.message);
+  };
   return (
-    <button className="absolute -bottom-24 left-1/2 -translate-x-1/2 rounded-xl border border-gray-900 bg-gray-700 px-3 py-1 text-2xl text-gray-200">
+    <button
+      className="absolute -bottom-24 left-1/2 -translate-x-1/2 rounded-xl border border-gray-900 bg-gray-700 px-3 py-1 text-2xl text-gray-200"
+      onClick={createGuest}
+    >
       Demo
     </button>
   );
