d2ef476deb832414287c80f527dc79aea016220e
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-06-27 22:39:47 -0700

    Add title as link for account-related forms

--- client/src/features/auth/components/Demo/index.tsx
+++ client/src/features/auth/components/Demo/index.tsx
@@ -20,7 +20,7 @@ export const Demo = () => {
   };
   return (
     <button
-      className="absolute -bottom-24 left-1/2 -translate-x-1/2 rounded-xl border border-gray-900 bg-gray-700 px-3 py-1 text-2xl text-gray-200"
+      className="absolute -bottom-36 left-1/2 -translate-x-1/2 rounded-xl border border-gray-900 bg-gray-600/70 px-3 py-1 text-xl text-gray-200"
       onClick={createGuest}
     >
       Demo
