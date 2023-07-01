b9e33fa10dc9b64047190452c8bfa33c4b11f308
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-07-01 14:21:57 -0700

    Style header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -4,28 +4,27 @@ import { Link, useLocation } from "react-router-dom";
 export const Header = () => {
   const location = useLocation();
   return (
-    <header className="flex  min-w-full items-center justify-between bg-gray-700/40 p-2">
+    <header className="flex min-w-full items-center justify-between border-b border-gray-700 bg-gray-900/20 p-2 text-gray-900 shadow-sm">
       <Link
-        className="flex items-center gap-1 text-2xl"
+        className=" pl-2 text-2xl font-medium"
         to="/home"
         aria-label="Navigate to home"
       >
-        <img src={dropletSVG} alt="Leaf" className="w-6" />
         Hydration
       </Link>
       <nav className="flex gap-4 px-2">
         <Link
           to="/plants"
-          className={`text-lg  ${
-            location.pathname !== "/plants" && "font-light"
+          className={`text-lg font-medium ${
+            location.pathname !== "/plants" && "text-gray-900/60"
           }`}
         >
           Plants
         </Link>
         <Link
           to="/user"
-          className={`text-lg  ${
-            location.pathname !== "/user" && "font-light"
+          className={`text-lg font-medium  ${
+            location.pathname !== "/user" && "text-gray-900/60"
           }`}
         >
           User
