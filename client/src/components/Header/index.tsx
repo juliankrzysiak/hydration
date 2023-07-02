89931627341491e7e42890ef11ad911f7223e503
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-07-01 20:35:41 -0700

    Fix text color of header

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Link, useLocation } from "react-router-dom";
 export const Header = () => {
   const location = useLocation();
   return (
-    <header className="flex min-w-full items-center justify-between border-b border-gray-700 bg-gray-900/20 p-2 text-gray-900 shadow-sm">
+    <header className="flex min-w-full items-center justify-between border-b border-gray-700 bg-gray-900/20 p-2 text-gray-950 shadow-sm">
       <Link
         className=" pl-2 text-2xl font-medium"
         to="/home"
@@ -15,7 +15,7 @@ export const Header = () => {
         <Link
           to="/plants"
           className={`text-lg font-medium ${
-            location.pathname !== "/plants" && "text-gray-900/60"
+            location.pathname !== "/plants" && "text-gray-900/70"
           }`}
         >
           Plants
@@ -23,7 +23,7 @@ export const Header = () => {
         <Link
           to="/user"
           className={`text-lg font-medium  ${
-            location.pathname !== "/user" && "text-gray-900/60"
+            location.pathname !== "/user" && "text-gray-900/70"
           }`}
         >
           User
