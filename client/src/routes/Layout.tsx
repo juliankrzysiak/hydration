093d5502e476acd06c3a3989b42bfe1c40e4271b
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-07-09 23:01:30 -0700

    Stop plants list duplication

--- client/src/routes/Layout.tsx
+++ client/src/routes/Layout.tsx
@@ -1,6 +1,7 @@
 import { Header } from "@/components/Header";
 import { Plants } from "@/features/calendar/routes/Plants";
 import { useDesktopWidth } from "@/hooks/useDesktopWidth";
+import { useLocation } from "react-router-dom";
 
 interface Props {
   children: React.ReactNode;
@@ -8,13 +9,14 @@ interface Props {
 
 export const Layout = ({ children }: Props) => {
   const [width] = useDesktopWidth();
+  const location = useLocation();
+
   return (
     <>
       <Header />
-      <main className="flex items-center justify-evenly p-10 font-['Nunito'] text-gray-950">
+      <main className="flex items-center justify-evenly p-6 font-['Nunito'] text-gray-950">
         {children}
-
-        {width && <Plants />}
+        {width && location.pathname === "/home" && <Plants />}
       </main>
     </>
   );
