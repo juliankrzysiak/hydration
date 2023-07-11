a34324da069be2ddd706a5521f135c3545278567
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-07-10 23:48:15 -0700

    Style filter components

--- client/src/features/calendar/components/Filter/FilterSelection.tsx
+++ client/src/features/calendar/components/Filter/FilterSelection.tsx
@@ -8,10 +8,10 @@ interface Props {
 export const FilterSelection = ({ name, id }: Props) => {
   const deletePlant = useFilterStore((state) => state.delete);
   return (
-    <div className="flex items-baseline gap-2 rounded-3xl bg-gray-900/50 px-3 py-1 font-bold">
-      <p className="text-gray-300">{name}</p>
-      <button className="text-gray-400" onClick={() => deletePlant(id)}>
-        x
+    <div className="flex items-center gap-3 rounded-2xl bg-gray-900/50 px-4 font-bold">
+      <p className=" text-lg text-gray-200">{name}</p>
+      <button className="text-lg text-gray-200" onClick={() => deletePlant(id)}>
+        X
       </button>
     </div>
   );
