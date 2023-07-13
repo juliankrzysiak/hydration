e9cd1fae1452e21b8f05da0fea9bfbe8dddb10fc
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-07-12 21:48:09 -0700

    Change loader background color to match

--- client/src/components/Loader/index.tsx
+++ client/src/components/Loader/index.tsx
@@ -2,7 +2,7 @@ import hourglass from "@/assets/hourglass.svg";
 
 export const Loader = () => {
   return (
-    <main className="h-screen w-screen bg-blue-300">
+    <main className="h-screen w-screen">
       <div className="fixed left-1/2 top-1/4 flex -translate-x-1/2 flex-col">
         <img className="animate-bounce" src={hourglass} alt="Hourglass" />
         <h3 className="text-2xl">Loading</h3>
