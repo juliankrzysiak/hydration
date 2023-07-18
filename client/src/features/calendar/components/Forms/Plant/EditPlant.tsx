d77f724b440f656fb29aaba5dd6b8df88e45a79d
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-07-17 19:21:34 -0700

    Fix edit plant going to login page

--- client/src/features/calendar/components/Forms/Plant/EditPlant.tsx
+++ client/src/features/calendar/components/Forms/Plant/EditPlant.tsx
@@ -30,7 +30,7 @@ export const EditPlant = (props: Props) => {
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["plants"] });
       useShowFormStore.setState({ editPlant: false });
-      navigate(-1);
+      navigate("/home");
       notify("success", "Plant edited");
     },
     onError: () => {
