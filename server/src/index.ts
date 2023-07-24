81dd0b577956a0d442b6e9b04be8200af09c2dde
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-07-23 23:59:27 -0700

    Refactor for server development

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,12 @@ import { app } from './app';
 import { logger } from './utils/logger';
 import { config } from './utils/config';
 
-app.listen(config.PORT, () => {
-	logger.info(`Server running on port ${config.PORT}`);
+const PORT = Number(config.PORT) || 3000;
+
+app.get('/version', (_req, res) => {
+	res.send('1');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+	logger.info(`Server running on port ${PORT}`);
 });
