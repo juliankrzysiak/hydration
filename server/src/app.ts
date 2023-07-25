2c60c3a96170ab8a47efcdba7f450d0872a2788c
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-07-24 19:26:47 -0700

    Test if only server is deployed

--- server/src/app.ts
+++ server/src/app.ts
@@ -15,7 +15,7 @@ app.use('/api/plants', plantsRouter);
 
 // For development use while setting up server
 app.get('/version', (_req, res) => {
-	res.send('1');
+	res.send('2');
 });
 
 app.use(middleware.unknownEndpoint);
