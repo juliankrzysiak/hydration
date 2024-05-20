ab4db8893d5b885152231e9929a94ddf20e002a4
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-05-20 11:53:09 -0700

    Add daisyui

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
+
 export default {
   content: [
     "./index.html",
@@ -9,5 +11,5 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
 };
