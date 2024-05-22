6f96ec2cc3cf667d167231f2b9ae316795945d3c
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-05-22 15:01:29 -0700

    Layout basic info for group

--- client/src/features/list/components/Groups/Info.tsx
+++ client/src/features/list/components/Groups/Info.tsx
@@ -6,7 +6,7 @@ import EditButton from "@/components/Buttons/EditButton";
 import DeleteButton from "@/components/Buttons/DeleteButton";
 
 type Props = {
-  group: Group;
+  group: Group | undefined;
 };
 
 export default function Info({ group }: Props) {
@@ -18,14 +18,36 @@ export default function Info({ group }: Props) {
 
   return (
     <div className="w-full">
-      <div className="flex w-full justify-between">
+      <div className="mb-4 flex w-full justify-between">
         <BackButton handleClick={exitInfo} />
         <div className="flex gap-4">
           <EditButton />
           <DeleteButton />
         </div>
       </div>
-      <h2>{group.name}</h2>
+      <div className="flex flex-col gap-4">
+        <h1 className="self-center text-2xl font-semibold underline">
+          {group.name}
+        </h1>
+        <div className="flex flex-col justify-around gap-4">
+          <div className="flex flex-col  gap-0">
+            <h2 className="text-xl">Schedule</h2>
+            <p>Every {group.schedule} days</p>
+          </div>
+          <div className="flex flex-col gap-0">
+            <h2 className="text-xl">Plants</h2>
+            <ul className="w-full">
+              {group.plants.map((plant) => {
+                return (
+                  <li key={plant.id} className="flex justify-between">
+                    <p>{plant.name}</p> <p>{plant.schedule}</p>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
