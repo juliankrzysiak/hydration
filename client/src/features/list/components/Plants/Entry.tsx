ee87456629a58c0df96ea0f225dadcaf584498aa
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-05-21 22:04:43 -0700

    Remove unnecessary key

--- client/src/features/list/components/Plants/Entry.tsx
+++ client/src/features/list/components/Plants/Entry.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 export const Entry = ({ name, id, watered }: Props) => {
   return (
-    <li key={id} className="flex h-fit justify-between gap-4 align-baseline">
+    <li className="flex h-fit justify-between gap-4 align-baseline">
       <button
         className="h-fit rounded-lg px-2 text-start text-xl font-medium text-gray-950 hover:bg-gray-700 hover:text-gray-200"
         onClick={() => useIdStore.setState({ id })}
