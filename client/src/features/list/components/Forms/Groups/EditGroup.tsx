3b6bf04574e3fbde6bb9cc190c35a25c06f570ac
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-05-22 22:17:32 -0700

    Rename and add query

--- client/src/features/list/components/Forms/Groups/EditGroup.tsx
+++ client/src/features/list/components/Forms/Groups/EditGroup.tsx
@@ -1,18 +1,34 @@
 import EditButton from "@/components/Buttons/EditButton";
 import { SinglePlantsContext } from "@/features/list/context";
 import { Group, Plant } from "@/types";
-import { useContext, useRef, useState } from "react";
+import { FormEvent, useContext, useRef, useState } from "react";
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { notify } from "@/utils";
+import { editGroup } from "@/features/list/api";
 
 type Props = {
   group: Group;
 };
 
-export default function EditForm({ group }: Props) {
+export default function EditGroup({ group }: Props) {
   const modalRef = useRef<HTMLDialogElement>(null);
   const selectRef = useRef<HTMLSelectElement>(null);
+
   const initialSinglePlants = useContext(SinglePlantsContext);
   const [groupPlants, setGroupPlants] = useState<Plant[]>(group.plants);
   const [singlePlants, setSinglePlants] = useState(initialSinglePlants);
+
+  const queryClient = useQueryClient();
+  const editGroupMutation = useMutation({
+    mutationFn: editGroup,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["plants"] });
+      notify("success", "Group edited");
+    },
+    onError: () => {
+      notify("error", "Could not edit group.");
+    },
+  });
 
   function showModal() {
     // The dialog is not unmounted when dialog is closed, so to emulate the default values on remounting, I set the state manually when dialog opened
@@ -49,40 +65,54 @@ export default function EditForm({ group }: Props) {
     setSinglePlants(updatedSinglePlants);
   }
 
+  function submitForm(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    const form = e.currentTarget;
+
+    const name = form.groupName.value;
+    const schedule = Number(form.schedule.value);
+
+    const data = {
+      id: group.id,
+      name,
+      schedule,
+    };
+
+    editGroupMutation.mutate(data);
+  }
+
   return (
     <>
       <EditButton handleClick={showModal} />
       <dialog ref={modalRef} className="modal">
         <div className="modal-box">
-          <form action="" className="flex flex-col gap-2">
-            <span className="flex items-center justify-between">
-              <h3 className="text-2xl">Edit Group</h3>
-              <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
-                <button className="">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="w-5"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M6 18 18 6M6 6l12 12"
-                    />
-                  </svg>
-                </button>
-              </form>
-            </span>
+          <form method="dialog" className="flex items-center justify-between">
+            <h3 className="text-2xl">Edit Group</h3>
+            <button>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18 18 6M6 6l12 12"
+                />
+              </svg>
+            </button>
+          </form>
+          <form className="flex flex-col gap-2 ">
             <label className="form-control w-full max-w-xs">
               <div className="label">
                 <span className="label-text">Name</span>
               </div>
               <input
                 type="text"
+                name="groupName"
                 className="input-bordered input w-full max-w-xs"
                 defaultValue={group.name}
               />
@@ -93,6 +123,7 @@ export default function EditForm({ group }: Props) {
               </div>
               <input
                 type="text"
+                name="schedule"
                 className="input-bordered input w-full max-w-xs"
                 defaultValue={group.schedule}
               />
