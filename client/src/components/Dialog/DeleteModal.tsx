57b761753d577a3ba6dde9371cd9ee3d42061c44
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-05-27 00:03:43 -0700

    Add default message to delete modal

--- client/src/components/Dialog/DeleteModal.tsx
+++ client/src/components/Dialog/DeleteModal.tsx
@@ -1,53 +1,59 @@
-import { ReactNode, forwardRef } from "react";
+import { FormEventHandler, ReactNode, forwardRef } from "react";
 
 type Props = {
   item: string;
-  handleSubmit: () => void;
+  message: string;
+  handleSubmit: FormEventHandler<HTMLFormElement>;
   children: ReactNode;
 };
 
 type Ref = HTMLDialogElement;
 
-const DeleteModal = forwardRef<Ref, Props>(({ item, children }, ref) => {
-  return (
-    <>
-      {children}
-      <dialog ref={ref} className="modal">
-        <form
-          method="dialog"
-          className="modal-box flex flex-col items-center"
-          action=""
-        >
-          <div className="mb-1 flex flex-col items-center">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="w-10 text-red-700"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
-              />
-            </svg>
-            <h3 className="text-xl font-bold">Delete {item}</h3>
-          </div>
-          <p className="">Are you sure about that?</p>
-          <div className="modal-action flex w-full justify-around">
-            <button className="btn" type="submit">
-              No, take me back
-            </button>
-            <button className="btn-error btn" type="submit">
-              Yes, delete
-            </button>
-          </div>
-        </form>
-      </dialog>
-    </>
-  );
-});
+const DeleteModal = forwardRef<Ref, Props>(
+  (
+    { item, message = "Are you sure about that?", handleSubmit, children },
+    ref
+  ) => {
+    return (
+      <>
+        {children}
+        <dialog ref={ref} className="modal">
+          <form
+            method="dialog"
+            className="modal-box flex flex-col items-center"
+            onSubmit={handleSubmit}
+          >
+            <div className="mb-1 flex flex-col items-center">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="w-10 text-red-700"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z"
+                />
+              </svg>
+              <h3 className="text-xl font-bold">Delete {item}</h3>
+            </div>
+            <p>{message}</p>
+            <div className="modal-action flex w-full justify-around">
+              <button className="btn" type="submit">
+                No, take me back
+              </button>
+              <button className="btn-error btn" type="submit">
+                Yes, delete
+              </button>
+            </div>
+          </form>
+        </dialog>
+      </>
+    );
+  }
+);
 
 export default DeleteModal;
