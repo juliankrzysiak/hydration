3a92a90bbe767ded34c1ade52dd4010417564b03
Julian Krzysiak <jkrzysiak13@gmail.com>
2024-05-28 20:20:07 -0700

    Refactor component

--- client/src/features/calendar/components/Water/ShowForm.tsx
+++ client/src/features/calendar/components/Water/ShowForm.tsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
-import { Plant } from "../../../../types";
-import { AddHistory } from "@/features/calendar/components/Forms/History/AddHistory";
-import { DeleteHistory } from "@/features/calendar/components/Forms/History/DeleteHistory";
 import { Buttons } from "@/components/Buttons";
+import { AddHistory } from "@/features/calendar/components/Forms/History/AddHistory";
+import { useState } from "react";
+import { Plant } from "@/types";
 
 interface Props {
   plants: Plant[];
@@ -10,13 +9,14 @@ interface Props {
 
 export const ShowForm = ({ plants }: Props) => {
   const [showAddForm, setShowAddForm] = useState(false);
-  // const [showDeleteForm, setShowDeleteForm] = useState(false);
 
-  const showCorrectForm = () => {
-    if (showAddForm)
-      return <AddHistory plants={plants} handleInput={setShowAddForm} />;
-    else return <Buttons accept={() => setShowAddForm(true)} />;
-  };
-
-  return <div className="mt-4 flex w-full gap-6">{showCorrectForm()}</div>;
+  return (
+    <div className="mt-4 flex w-full gap-6">
+      {showAddForm ? (
+        <AddHistory plants={plants} handleInput={setShowAddForm} />
+      ) : (
+        <Buttons accept={() => setShowAddForm(true)} />
+      )}
+    </div>
+  );
 };
